73da7e6d931a51212add8950f652e3e1ee570ff3
mqs <Wait@qq.com>
2023-03-07 23:54:37 +0800

    gorm配置

--- config/conf_mysql.go
+++ config/conf_mysql.go
@@ -1,10 +1,18 @@
 package config
 
+import "strconv"
+
+// 设置结构体，将yaml中的配置映射到结构体中
 type Mysql struct {
 	Host     string `yaml:"host"`
 	Port     int    `yaml:"port"`
+	Config   string `yaml:"config"` //高级配置,例如 charset
 	DB       string `yaml:"db"`
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
 	LogLevel string `yaml:"log_level"` //日志等级，debug就是输出全部sql，dev,nelease
+}
+
+func (m Mysql) Dsn() string {
+	return m.User + ":" + m.Password + "@tcp(" + m.Host + ":" + strconv.Itoa(m.Port) + ")/" + m.DB + "?" + m.Config
 }
--- config/enter.go
+++ config/enter.go
@@ -1,5 +1,6 @@
 package config
 
+// 设置结构体，将yaml中的配置映射到结构体中
 type Config struct {
 	Mysql  Mysql  `yaml:"mysql"`
 	Logger Logger `yaml:"logger"`
