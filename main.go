a37921f0ea0e3f8642a0622509f2599c441f3a98
mqs <Wait@qq.com>
2023-03-17 21:22:24 +0800

    日志初始化配置

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"gvb_server/core"
 	"gvb_server/global"
 )
@@ -8,6 +9,16 @@ import (
 func main() {
 	// 读取配置文件
 	core.InitConf()
+	// 初始化日志
+	global.Log = core.InitLogger()
+	global.Log.Warnln("嘻嘻嘻")
+	global.Log.Error("嘻嘻嘻")
+	global.Log.Infof("嘻嘻嘻")
+
+	logrus.Warnln("嘻嘻嘻")
+	logrus.Error("嘻嘻嘻")
+	logrus.Infof("嘻嘻嘻")
+
 	// 连接数据库
 	global.DB = core.InitGorm()
 }
